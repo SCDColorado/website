78c9ac8024e8f1a8958135b7ac4957b96dc6879d
SCDColorado <scdcolorado@gmail.com>
2022-10-11 13:28:12 -0600

    initial commit

---
+++ scdcolorado.org/Monthly_Regional_Dance_files/Monthly_Regional_Dance.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20150504
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Monthly_Regional_Dance_files/Monthly_Regional_DanceMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
